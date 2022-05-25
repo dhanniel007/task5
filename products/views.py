33d03337cc1f8762bfe1c01c75a826bbb8c8cb0f
oluwaseun dairo <theway.christ@gmail.com>
2022-05-25 02:32:34 +0100

    task 5 submissions

--- products/views.py
+++ products/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
 
 def watercurls(request):
-    return HttpResponse("<h1> Water Curls</h1>")
+    return render(request, "products/watercurls.html")
 def bonestraight(request):
-    return HttpResponse("<h1> Bone Straight</h1>")
+    return render(request, "products/bonestraight.html")
 def pinkycurls(request):
-    return HttpResponse("<h1> Pinky Curls</h1>")  
+    return render(request, "products/pinkycurls.html")  
 def products(request):
-    return HttpResponse("<h1> Product Page</h1>")
+    return render(request, "products/products.html")
